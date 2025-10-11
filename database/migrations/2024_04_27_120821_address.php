8aee41d34a00c3461edeb80eb9871a77a17fd99b
Mohammad Moin Uddin Moin <mohammadmoinuddin2491@gmail.com>
2025-10-11 20:01:59 +0600

    password_resets, failed_jobs, users, admin, vendor, address, and brand tables

---
+++ database/migrations/2024_04_27_120821_address.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->Integer('address_number');
+            $table->text('address');
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+};
