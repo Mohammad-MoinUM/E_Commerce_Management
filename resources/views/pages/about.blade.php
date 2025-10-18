6b75aa617d7cc3f44670cd4d16e2aa9f05d45894
Mohammad Moin Uddin Moin <mohammadmoinuddin2491@gmail.com>
2025-10-19 00:14:54 +0600

    docs(about): update about page content and branding
    Update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,17 @@
 @extends('layouts.app')
 @section('content')
-@include('partial.slider',['categories' => App\Models\Category::all()])
+@include('partial.slider',["categories" => App\Models\Category::all()])
 <hr/>
 <div class="about-section margin-top">
     <div class="container">
         <div class="row">
             <div class="col-md-6">
                 <h1>About Shop</h1>
-                <p>Minimatic Solutions Content Coming Soon.... 
-                </p>
+                <p>Discover quality products at competitive prices with secure payments and fast delivery.</p>
             </div>
             <div class="col-md-6" align="center">
                 <img style="border-radius:50%;" src="image/logo.jpg" width="50%"/>
-                <h1 class="logotext">Minimatic Solutions</h1>
+                <h1 class="logotext">Moin's_Online_Marketing</h1>
             </div>
         </div>
     </div>
